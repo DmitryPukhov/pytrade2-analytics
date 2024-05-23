e8032e839139e665092d5b73c609d640e1fe7f76
dmitriy.pukhov <pukhov@gnivc.ru>
2024-05-23 19:31:37 +0300

    download_data.sh parameterize days

--- download_data.sh
+++ download_data.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Download new candles to data folder
+days=${1:-2}
 
 PROJECT_DIR=$(pwd)
 PYTRADE2_DIR=$PROJECT_DIR/pytrade2
 DATA_DIR=$PROJECT_DIR/data
 
-echo "Downloading new candles to $DATA_DIR"
+echo "Downloading new candles for days:$days  to $DATA_DIR"
 today=$(date '+%Y-%m-%d')
 
 cd $PYTRADE2_DIR/pytrade2
-python DataDownloadApp.py --pytrade2.data.dir $DATA_DIR --to $today --days 2
+python DataDownloadApp.py --pytrade2.data.dir $DATA_DIR --to $today --days $days
